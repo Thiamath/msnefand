7881d4ca73e0d3a5e0505ab1a0b25e2b70afea61
Thiamath <thiamath@gmail.com>
2020-04-29 14:07:48 +0200

    Project configuration.

---
+++ src/main/java/com/thiamath/msnefand/MsnefandApplication.java
@@ -0,0 +1,13 @@
+package com.thiamath.msnefand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnefandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsnefandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiamath/msnefand/MsnefandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiamath.msnefand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsnefandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
